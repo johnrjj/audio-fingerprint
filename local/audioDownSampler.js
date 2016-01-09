84ddbc86d34ec6c6119e83e2c85a975be082663f
John J <johnrjj@gmail.com>
2016-01-09 15:29:29 -0500

    Added styling and comments

--- local/audioDownSampler.js
+++ local/audioDownSampler.js
@@ -5,19 +5,20 @@ var decimator = require('./audioDecimator');
 var dsp = require('./dsp');
 
 module.exports = function(signal, origFrequency, targetFrequency) {
-  if(origFrequency < targetFrequency) {
+  if (origFrequency < targetFrequency) {
     throw 'Target frequency must be smaller than original frequency to downsample';
-  }
-  else if(origFrequency % targetFrequency != 0) {
+  } else if (origFrequency % targetFrequency != 0) {
     throw 'Frequencies must be divisible by an integer factor.';
   }
   // Create low-pass filter to get signal ready for downsampling
-  var filter = new dsp.IIRFilter(dsp.LOWPASS, targetFrequency, 1, origFrequency);
+  // tldr: IIRFilter(filtertype, cutoff frequency, resonance, original frequency)
+  let filter = new dsp.IIRFilter(dsp.LOWPASS, targetFrequency, 1, origFrequency);
+  // Apply low-pass filter in place
   filter.process(signal);
   //Decimate signal to factor we want
-  var decimateFactor = parseInt((origFrequency/targetFrequency));
-  var decimatedSignal = decimator(signal, decimateFactor);
-  // Applying hamming window to prevent spectrum leakage
+  let decimateFactor = parseInt((origFrequency / targetFrequency));
+  let decimatedSignal = decimator(signal, decimateFactor);
+  // Applying hamming window to prevent spectrum leakage, in place
   windowFilter.applyWindowFilter('HAMMING', decimatedSignal);
   // Return operated-on signal
   return decimatedSignal;
--- local/audioWindow.js
+++ local/audioWindow.js
@@ -2,13 +2,13 @@
 var _ = require('lodash');
 
 module.exports = {
-  computeWindowFilterCoef: function(length, index) {
-    return 0.54 - 0.46 * Math.cos(2*Math.PI * index / (length - 1));
+  computeWindowFilterCoefForIndex: function(length, index) {
+    return 0.54 - 0.46 * Math.cos(2 * Math.PI * index / (length - 1));
   },
   applyWindowFilter: function(filter, source) {
     let length = source.length;
     _.forEach(source, (element, index) => {
-      element *= this.computeWindowFilterCoef(length, index);
+      element *= this.computeWindowFilterCoefForIndex(length, index);
     });
   }
 };
