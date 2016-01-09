65cdc7457cb51118a333f7d00671e6e58897de80
John J <johnrjj@gmail.com>
2016-01-09 15:22:09 -0500

    beautify audio recording webworker

--- recorderWebWorker.js
+++ recorderWebWorker.js
@@ -5,7 +5,7 @@ var recLength = 0,
 
 
 self.onmessage = function(e) {
-  switch(e.data.command){
+  switch (e.data.command) {
     case 'init':
       init(e.data.config);
       break;
@@ -24,58 +24,60 @@ self.onmessage = function(e) {
   }
 };
 
-function init(config){
+function init(config) {
   sampleRate = config.sampleRate;
   console.log('in webworker');
 }
 
-function record(inputBuffer){
+function record(inputBuffer) {
   recBuffersL.push(inputBuffer[0]);
   recBuffersR.push(inputBuffer[1]);
   recLength += inputBuffer[0].length;
 }
 
-function exportWAV(type){
+function exportWAV(type) {
   var bufferL = mergeBuffers(recBuffersL, recLength);
   var bufferR = mergeBuffers(recBuffersR, recLength);
   var interleaved = interleave(bufferL, bufferR);
   var dataview = encodeWAV(interleaved);
-  var audioBlob = new Blob([dataview], { type: type });
+  var audioBlob = new Blob([dataview], {
+    type: type
+  });
 
   self.postMessage(audioBlob);
 }
 
 function getBuffer() {
   var buffers = [];
-  buffers.push( mergeBuffers(recBuffersL, recLength) );
-  buffers.push( mergeBuffers(recBuffersR, recLength) );
+  buffers.push(mergeBuffers(recBuffersL, recLength));
+  buffers.push(mergeBuffers(recBuffersR, recLength));
   self.postMessage(buffers);
 }
 
-function clear(){
+function clear() {
   recLength = 0;
   recBuffersL = [];
   recBuffersR = [];
 }
 
-function mergeBuffers(recBuffers, recLength){
+function mergeBuffers(recBuffers, recLength) {
   var result = new Float32Array(recLength);
   var offset = 0;
-  for (var i = 0; i < recBuffers.length; i++){
+  for (var i = 0; i < recBuffers.length; i++) {
     result.set(recBuffers[i], offset);
     offset += recBuffers[i].length;
   }
   return result;
 }
 
-function interleave(inputL, inputR){
+function interleave(inputL, inputR) {
   var length = inputL.length + inputR.length;
   var result = new Float32Array(length);
 
   var index = 0,
     inputIndex = 0;
 
-  while (index < length){
+  while (index < length) {
     result[index++] = inputL[inputIndex];
     result[index++] = inputR[inputIndex];
     inputIndex++;
@@ -83,20 +85,20 @@ function interleave(inputL, inputR){
   return result;
 }
 
-function floatTo16BitPCM(output, offset, input){
-  for (var i = 0; i < input.length; i++, offset+=2){
+function floatTo16BitPCM(output, offset, input) {
+  for (var i = 0; i < input.length; i++, offset += 2) {
     var s = Math.max(-1, Math.min(1, input[i]));
     output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
   }
 }
 
-function writeString(view, offset, string){
-  for (var i = 0; i < string.length; i++){
+function writeString(view, offset, string) {
+  for (var i = 0; i < string.length; i++) {
     view.setUint8(offset + i, string.charCodeAt(i));
   }
 }
 
-function encodeWAV(samples){
+function encodeWAV(samples) {
   var buffer = new ArrayBuffer(44 + samples.length * 2);
   var view = new DataView(buffer);
 
