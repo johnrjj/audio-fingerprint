9da449a0ea5dde9f43f7db56fcb55b1471a43f7f
John J <johnrjj@gmail.com>
2016-01-14 21:37:52 -0500

    Renamed timeoffset vars

--- local/audio-timeoffset.js
+++ local/audio-timeoffset.js
@@ -5,14 +5,14 @@ let _ = require('lodash');
 
 // Takes an array of audiosample windows and applies a timeoffset to the audioArray
 // based on the window size, sample rate, and provided time offset
-module.exports = function x(audioArray, audioWindowSize, sampleRate, timeOffset) {
+module.exports = function x(audioWindows, audioWindowSize, sampleRate, timeOffset) {
   // For each window...
-  return _.map(audioArray, (chosenFeaturesPerWindow, index) => {
+  return _.map(audioWindows, (audioWindow, index) => {
     // Calc cur time for window
     let time = ((index * audioWindowSize / sampleRate) + timeOffset);
     // Apply time to each point in window. (immutable tho, hence clone)
-    return _.map(chosenFeaturesPerWindow, (audioFeature) => {
-      let temp = _.cloneDeep(audioFeature);
+    return _.map(audioWindow, (audioPointInWindow) => {
+      let temp = _.cloneDeep(audioPointInWindow);
       temp.time = time;
       return temp;
     });
