daf6134a78d4c178bf329e9723879034fd4231f7
John J <johnrjj@gmail.com>
2016-02-21 13:39:30 -0500

    frontend talking to api

--- api/server.js
+++ api/server.js
@@ -10,7 +10,14 @@ let url = 'mongodb://localhost:27017/musicindexer1';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var port = process.env.PORT || 8001;
 var router = express.Router();
 
 app.use('/api', router);
@@ -24,12 +31,12 @@ router.get('/', function(req, res) {
 router.route('/fingerprint')
     .post(function(req, res) {
       console.log(req.body);
-      MongoClient.connect(url, function(err, db) {
-        var collection = db.collection('fingerprints');
-      });
+      // MongoClient.connect(url, function(err, db) {
+      //   var collection = db.collection('fingerprints');
+      // });
       res.json({
         'hue': 'hue',
       });
     });
-    
+
 console.log('api running on port:' + port);
