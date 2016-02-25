63bb8c35d3dc56195430d0840c3fb8c92a2e9901
John J <johnrjj@gmail.com>
2016-02-25 09:37:27 -0500

    Updates, fine tuning

--- tools/index-music-library.js
+++ tools/index-music-library.js
@@ -9,29 +9,26 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var fingerprinter = require('../lib/audio-fingerprint');
+var collection = {};
 
 function dbInteract(objArray, dbUrl, callback) {
-  let url = dbUrl;
-  url = 'mongodb://localhost:27017/musicindexer1';
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+
     // db.collection('fingerprints').drop();
-    var collection = db.collection('fingerprints');
+    // var collection = db.collection('fingerprints');
     var bulk = collection.initializeUnorderedBulkOp();
+    console.log('now what');
     _.forEach(objArray, obj => {
       bulk.insert(obj);
     });
     let result = bulk.execute((err, result) => {
       if(err) throw err;
-      db.close();
+      // db.close();
       callback(result);
     });
-  });
-}
+  }
 
 function globAudioFiles(startingDirectory, options, audioFileTypes) {
-  return glob("../lib//audio_files/tswift/19892/**/*", options, (er, files) => {
+  return glob("../lib//audio_files/top40/**/*", options, (er, files) => {
     var filtered = _.filter(files, file => {
       if (~file.indexOf('.mp3')) {
         return true;
@@ -49,8 +46,10 @@ function analyzeFile(path, cb) {
   fs.readFile(path,  function (err, data ) {
     console.log('reading data');
     context.decodeAudioData(data, function(decodedData) {
+      console.log('in decode)');
       var signal = decodedData.getChannelData(0);
       var res = fingerprinter(signal, 44100, 11025, path);
+      console.log('past fingerprinter');
       dbInteract(res, 'mongodb://localhost:27017/musicindexer', (resFromDb) => {
         console.log('done uploading' + path);
         return;
@@ -60,4 +59,15 @@ function analyzeFile(path, cb) {
   });
 };
 
-globAudioFiles(null, null, null);
+MongoClient.connect('mongodb://localhost:27017/musicindexer1', function(err, db) {
+  console.log('in db');
+  let url = '';
+  url = 'mongodb://localhost:27017/musicindexer1';
+      // db.collection('fingerprints').drop();
+  collection = db.collection('fingerprints');
+      console.log('connected');
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+  globAudioFiles(null, null, null);
+
+});
